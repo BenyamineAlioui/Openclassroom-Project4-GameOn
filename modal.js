e4121b257895a77930af94bd23529237503110fc
Benyamine Alioui <benyamine.alioui@outlook.com>
2022-01-18 16:05:35 +0100

    validation du prenom nom ok, manque le reste !

--- modal.js
+++ modal.js
@@ -23,123 +23,144 @@ function launchModal() {
 // On récupère le btn close de la modal
 const btnClose = document.querySelector(".close"); 
 
-// On écoute l'évenement click
+// On écoute l'évenement click avec la fonction closeModal
 btnClose.addEventListener('click', closeModal);
 
-//On crée la fonction closeModal
+//On crée la fonction closeModal qui permet de fermer la modal
 function closeModal () {
   modalbg.style.display = "none";
 }
-  
+
+// On écoute l'évenement click avec la fonction refresh
+btnClose.addEventListener('click', refresh);
+
+//On crée la fonction refresh pour actualiser la pages a la fermeture de la modal pour avoir une modal vide à sa reouverture
+function refresh () {
+  location.reload(true);
+}
+
 //On crée une variable en recuperant notre formulaire
 let myFormValidation = document.getElementById("modalForm") 
 
-//On crée une fonction 
+//On crée une fonction pour la validation du formulaire
 myFormValidation.addEventListener('submit', function(e) {
-
+  
   // On récupère tout les elements du formulaire
   let firstName = document.querySelector("#first");
   let lastName = document.querySelector("#last");
   let email = document.querySelector(".formData .email");
   let birthdate = document.querySelector("#birthdate");
   let quantity = document.querySelector("#quantity");
-  let locationn = document.querySelector(".radio");
+  let locatiOn1 = document.querySelector("#location1");
+  let locatiOn2 = document.querySelector("#location2");
+  let locatiOn3 = document.querySelector("#location3");
+  let locatiOn4 = document.querySelector("#location4");
+  let locatiOn5 = document.querySelector("#location5");
+  let locatiOn6 = document.querySelector("#location6");
   let userConditions = document.querySelector("#checkbox1");
-  let myError = document.querySelector(".error");
-  let myBorder = document.querySelector(".text-control");
-  // Expression reguliere
-  let validEmailRegex = /(?:^|\s)[\w!#$%&'*+/=?^`{|}~-](\.?[\w!#$%&'*+/=?^`{|}~-]+)*@\w+[.-]?\w*\.[a-zA-Z]{2,3}\b/;
-  let birthdateRegEx = /(Date of birth:|Birthday:)\s+(?:0[1-9]|1[012])[-/.](?:0[1-9]|[12][0-9]|3[01])[-/.](?:19\d{2}|20[01][0-9]|2020)\b/;
 
-    if (firstName.value == "") {
-      myError.innerHTML = "Le champ prénom est requis";
-      myError.style.color = "red";
-      myError.style.fontSize = "13px";
-      myBorder.style.borderColor = "red";
-      e.preventDefault();
-    }else if ( 0 < firstName.value.length <= 2 ) {
+  // Expression reguliere
+  let validEmailRegex = /.+\@.+\..+/;
+  let validBirthdateRegex = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+  let validQuantityRegex = /^[0-9]+$/;
+  
+   
+    if (firstName.value.length < 2) {
+      let myError = document.querySelector(".errorFirstName");
+      let myBorder = document.querySelector(".text-control1");
       myError.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
       myError.style.color = "red";
       myError.style.fontSize = "13px";
       myBorder.style.borderColor = "red";
       e.preventDefault();
-    } else {
-      myBorder.style.borderColor = "green";
     }
 
-    if (lastName.value == "") {
-      myError.innerHTML = "Le champ nom est requis.";
-      myError.style.color = "red";
-      myError.style.fontSize = "13px";
-      myBorder.style.borderColor = "red";
-      e.preventDefault();
-    }else if ( 0 < lastName.value.length <= 2 ) {
-      let myError = document.querySelector(".error");
+    if (lastName.value.length < 2) {
+      myError = document.querySelector(".errorLastName");
+      myBorder = document.querySelector(".text-control2");
       myError.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
-      myError.style.color = "red";
-      myError.style.fontSize = "13px";
-      myBorder.style.borderColor = "red";
-      e.preventDefault();
-    } else {
-      myBorder.style.borderColor = "green";
-    }
-
-    if (email.value === "") {
-      myError.innerHTML = "Le champ email est requis.";
-      myError.style.color = "red";
-      myError.style.fontSize = "13px";
-      myBorder.style.borderColor = "red";
-      e.preventDefault();
-    }else if (validEmailRegex.test(email.value) === false) {
-      let myError = document.querySelector(".error");
-      myError.innerHTML = "Adresse email non valide.";
-      myError.style.color = "red";
-      myError.style.fontSize = "13px";
-      myBorder.style.borderColor = "red";
-      e.preventDefault();
-    } 
-
-    if (birthdate.value === "") {
-      myError.innerHTML = "Le champ date de naissance est requis";
-      myError.style.color = "red";
-      myError.style.fontSize = "13px";
-      myBorder.style.borderColor = "red";
-      e.preventDefault();
-    }else if (birthdateRegEx.test(birthdate.value) === false) {
-      let myError = document.querySelector(".error");
-      myError.innerHTML = ""
-      myError.style.color = "red";
-      myError.style.fontSize = "13px";
-      myBorder.style.borderColor = "red";
-      e.preventDefault();
-    } else {
-      
-    }
-
-    if (quantity.value == "") {
-      myError.innerHTML = "Ce champs est requis";
       myError.style.color = "red";
       myError.style.fontSize = "13px";
       myBorder.style.borderColor = "red";
       e.preventDefault();
     }
     
-    if (location.value == false) {
-      let myError = document.querySelector(".error");
+    if (validEmailRegex.test(email.value) === false) {
+      myError = document.querySelector(".errorEmail");
+      myBorder = document.querySelector(".text-control3");
+      myError.innerHTML = "L'adresse email saisie n'est pas valide.";
+      myError.style.color = "red";
+      myError.style.fontSize = "13px";
+      myBorder.style.borderColor = "red";
+      e.preventDefault();
+    }
+
+    if (validBirthdateRegex.test(birthdate.value) === false) {
+      myError = document.querySelector(".errorBirthdate");
+      myBorder = document.querySelector(".text-control4");
+      myError.innerHTML = "La date de naissance saisie n'est pas valide."
+      myError.style.color = "red";
+      myError.style.fontSize = "13px";
+      myBorder.style.borderColor = "red";
+      e.preventDefault();
+    }
+    
+    if (validQuantityRegex.test(quantity.value) === false) {
+      myError = document.querySelector(".errorQuantity");
+      myBorder = document.querySelector(".text-control5");
+      myError.innerHTML = "Ce champs n'est pas valide."
+      myError.style.color = "red";
+      myError.style.fontSize = "13px";
+      myBorder.style.borderColor = "red";
+      e.preventDefault();
+    }
+    
+    if (locatiOn1.checked === false) {
+      myError = document.querySelector(".errorLocation1");
+      myError.innerHTML = "Ce champs est requis";
+      myError.style.color = "red";
+      myError.style.fontSize = "13px";
+      e.preventDefault();
+    } else if (locatiOn2.checked === false) {
+      myError = document.querySelector(".errorLocation2");
+      myError.innerHTML = "Ce champs est requis";
+      myError.style.color = "red";
+      myError.style.fontSize = "13px";
+      e.preventDefault();
+    } else if (locatiOn3.checked === false) {
+      myError = document.querySelector(".errorLocation3");
+      myError.innerHTML = "Ce champs est requis";
+      myError.style.color = "red";
+      myError.style.fontSize = "13px";
+      e.preventDefault();
+    } else if (locatiOn4.checked === false) {
+      myError = document.querySelector(".errorLocation4");
+      myError.innerHTML = "Ce champs est requis";
+      myError.style.color = "red";
+      myError.style.fontSize = "13px";
+      e.preventDefault();
+    } else if (locatiOn5.checked === false) {
+      myError = document.querySelector(".errorLocation5");
+      myError.innerHTML = "Ce champs est requis";
+      myError.style.color = "red";
+      myError.style.fontSize = "13px";
+      e.preventDefault();
+    } else if (locatiOn6.checked === false) {
+      myError = document.querySelector(".errorLocation6");
       myError.innerHTML = "Ce champs est requis";
       myError.style.color = "red";
       myError.style.fontSize = "13px";
       e.preventDefault();
     } 
 
-    if (userConditions.value == false) {
+    if (userConditions.checked === false) {
+      myError = document.querySelector(".errorUC");
       myError.innerHTML = "Vous devez vérifier que vous acceptez les termes et conditions.";
       myError.style.color = "red";
       myError.style.fontSize = "13px";
       e.preventDefault();
-    } else {
-      
     }
+
 })
 
 
